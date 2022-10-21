8b34a6aab03d85bde45390595d7a13910da686c8
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2022-10-21 23:07:24 +0500

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 import django
+
+from django.core.asgi import get_asgi_application
+wsgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter,URLRouter
 from mysite.consumers import MyMqttConsumer
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import mysite.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -19,7 +22,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
-        'http': get_asgi_application(),
+        'http': wsgi_app,
         'mqtt': MyMqttConsumer.as_asgi(),  
         "websocket": AuthMiddlewareStack(
             URLRouter(
