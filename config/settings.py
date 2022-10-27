dd5bae2231fbb38dbf7cba8245de129d673c1bb9
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2022-10-27 17:45:12 +0500

    Update settings.py
    static root

--- config/settings.py
+++ config/settings.py
@@ -217,7 +217,7 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static')
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
