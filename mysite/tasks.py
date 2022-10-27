7802cd054f28a3dd7b4406163610fccd691e41f6
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2022-10-27 11:50:45 +0500

    Update tasks.py
    time sleep task

--- mysite/tasks.py
+++ mysite/tasks.py
@@ -12,15 +12,10 @@ client.username_pw_set(username="bobur",password="bobur")
 
 @app.task() 
 def mqtt_task_scheduale(status,channels,sensor_status=None):
-    print("11111111")
     client.connect('localhost',1883,60)
-    print("222222")
-    print(channels,"eeeeeee channels")
     for chan in channels:
-        topic_name = Channel.objects.get(id=chan)
-        print("33333")
-        client.publish(topic_name.topic_name,status)
+        channel = Channel.objects.get(id=chan)
+        client.publish(channel.topic_name,status)
         time.sleep(1)
-        print("444444")
     client.disconnect()
     return {"result":True}
