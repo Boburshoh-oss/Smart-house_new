8fdaf04f252313fbff81bd852580137a587572eb
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2022-10-27 11:49:37 +0500

    Update tasks.py
    time sleep

--- mysite/tasks.py
+++ mysite/tasks.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from django.utils import dateformat
-
+import time
 from config.celery import app
 from mysite.models import Channel
 
@@ -20,6 +20,7 @@ def mqtt_task_scheduale(status,channels,sensor_status=None):
         topic_name = Channel.objects.get(id=chan)
         print("33333")
         client.publish(topic_name.topic_name,status)
+        time.sleep(1)
         print("444444")
     client.disconnect()
     return {"result":True}
